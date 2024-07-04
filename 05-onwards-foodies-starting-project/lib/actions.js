1bbdd3e56b88b75b0de5bd953057af483b00467b
sukyung6999 <sukyung6999@gmail.com>
2024-07-04 20:24:28 +0900

    add: 서버 측 유효성 검사

--- 05-onwards-foodies-starting-project/lib/actions.js
+++ 05-onwards-foodies-starting-project/lib/actions.js
@@ -1,6 +1,12 @@
 'use server';
 
+import { redirect } from 'next/navigation';
 import { saveMeal } from './meals';
+import { isValidElement } from 'react';
+
+function inValidText(text) {
+  return !meal.title || meal.title.trim() === '';
+}
 
 export async function shareMeal(formData) {
   const meal = {
@@ -11,5 +17,20 @@ export async function shareMeal(formData) {
     creator: formData.get('name'),
     creator_email: formData.get('email'),
   };
+
+  if (
+    inValidText(meal.title) ||
+    isValidElement(meal.summary) ||
+    isValidElement(meal.instructions) ||
+    isValidElement(meal.creator) ||
+    isValidElement(meal.creator_email) ||
+    !meal.creator_email.includes('@') ||
+    !meal.image ||
+    meal.images.size === 0
+  ) {
+    throw new Error('Invalid Input');
+  }
+
   await saveMeal(meal);
+  redirect('/meals');
 }
