c96d99df8ab2293f312b3ee9e8045936208e26bf
sukyung6999 <sukyung6999@gmail.com>
2024-07-10 07:41:29 +0900

    add: 백엔드 요청보내기

--- practice02/app/(content)/news/page.js
+++ practice02/app/(content)/news/page.js
@@ -1,11 +1,50 @@
+'use client';
+
 import { DUMMY_NEWS } from '@/dummy-news';
 import NewsList from '@/components/news-list';
+import { useEffect, useState } from 'react';
 
 export default function NewsPage() {
+  const [news, setNews] = useState();
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState();
+
+  useEffect(() => {
+    setIsLoading(true);
+
+    async function FetchNews() {
+      const response = await fetch('http://localhost:8080/news');
+
+      if (!response.ok) {
+        setIsLoading(false);
+        setError('Failed to fetch');
+      }
+
+      const news = response.json();
+      setIsLoading(false);
+      setNews(news);
+    }
+
+    FetchNews();
+  }, []);
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+
+  if (error) {
+    return <p>{error}</p>;
+  }
+
+  let newsContent;
+
+  if (news) {
+    return <NewsList news={DUMMY_NEWS} />;
+  }
   return (
     <>
       <h1>News Page</h1>
-      <NewsList news={DUMMY_NEWS} />
+      {newsContent}
     </>
   );
 }
