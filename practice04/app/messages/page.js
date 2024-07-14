1fe2ca40e0f45bbf61694bdbdabb36c7e39556b9
sukyung6999 <sukyung6999@gmail.com>
2024-07-14 15:23:29 +0900

    add: revalidateTag 함수

--- practice04/app/messages/page.js
+++ practice04/app/messages/page.js
@@ -1,12 +1,13 @@
 import Messages from '@/components/messages';
-import { unstable_noStore } from 'next/cache';
 
-// export const revalidate = 5; 5초마다 재검증
-// export const dynamic = 'force-dynamic'; 캐시를 막음
+export const dynamic = 'force-dynamic';
 
 export default async function MessagesPage() {
-  unstable_noStore(); // 캐시를 막음
-  const response = await fetch('http://localhost:8080/messages');
+  const response = await fetch('http://localhost:8080/messages', {
+    next: {
+      tags: ['msg'],
+    },
+  });
   const messages = await response.json();
 
   if (!messages || messages.length === 0) {
