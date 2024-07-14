63b882153efe3cdab428defcb1732f1b33b0df6b
sukyung6999 <sukyung6999@gmail.com>
2024-07-14 16:41:31 +0900

    add: 데이터 캐시 무효화

--- practice04/app/messages/new/page.js
+++ practice04/app/messages/new/page.js
@@ -1,7 +1,7 @@
 import { redirect } from 'next/navigation';
 
 import { addMessage } from '@/lib/messages';
-import { revalidateTag } from 'next/cache';
+import { revalidatePath, revalidateTag } from 'next/cache';
 
 export default function NewMessagePage() {
   async function createMessage(formData) {
@@ -9,7 +9,8 @@ export default function NewMessagePage() {
 
     const message = formData.get('message');
     addMessage(message);
-    // revalidateTag('msg');
+    // revalidatePath('/messages');
+    revalidateTag('msg');
     redirect('/messages');
   }
 
--- practice04/app/messages/page.js
+++ practice04/app/messages/page.js
@@ -1,14 +1,14 @@
 import Messages from '@/components/messages';
 import { getMessages } from '@/lib/messages';
 
-export default function MessagesPage() {
+export default async function MessagesPage() {
   // const response = await fetch('http://localhost:8080/messages', {
   //   next: {
   //     tags: ['msg'],
   //   },
   // });
   // const messages = await response.json();
-  const messages = getMessages();
+  const messages = await getMessages();
 
   if (!messages || messages.length === 0) {
     return <p>No messages found</p>;
