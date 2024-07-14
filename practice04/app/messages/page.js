7ae66bd455cbffa3db0c6cb6e6559d6ffe0dfa76
sukyung6999 <sukyung6999@gmail.com>
2024-07-14 13:42:55 +0900

    add: 데이터 캐시 방법2_페이지 내에서 설정

--- practice04/app/messages/page.js
+++ practice04/app/messages/page.js
@@ -1,8 +1,11 @@
 import Messages from '@/components/messages';
+import { unstable_noStore } from 'next/cache';
 
-export const dynamic = 'force-dynamic';
+// export const revalidate = 5; 5초마다 재검증
+// export const dynamic = 'force-dynamic'; 캐시를 막음
 
 export default async function MessagesPage() {
+  unstable_noStore(); // 캐시를 막음
   const response = await fetch('http://localhost:8080/messages');
   const messages = await response.json();
 
