12990d8dbb092709793d8beec00075546ff0c316
sukyung6999 <sukyung6999@gmail.com>
2024-07-14 16:10:18 +0900

    add: cache 함수

--- practice04/app/messages/page.js
+++ practice04/app/messages/page.js
@@ -1,14 +1,14 @@
 import Messages from '@/components/messages';
+import { getMessages } from '@/lib/messages';
 
-export const dynamic = 'force-dynamic';
-
-export default async function MessagesPage() {
-  const response = await fetch('http://localhost:8080/messages', {
-    next: {
-      tags: ['msg'],
-    },
-  });
-  const messages = await response.json();
+export default function MessagesPage() {
+  // const response = await fetch('http://localhost:8080/messages', {
+  //   next: {
+  //     tags: ['msg'],
+  //   },
+  // });
+  // const messages = await response.json();
+  const messages = getMessages();
 
   if (!messages || messages.length === 0) {
     return <p>No messages found</p>;
